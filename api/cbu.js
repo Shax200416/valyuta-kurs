371fdabe4e2a772dc8d509785542ecee4550c0b2
Shoxrux Yaxshilikov <shaxruh6114@gmail.com>
2026-07-28 17:05:33 +0600

    first commit

---
+++ api/cbu.js
@@ -0,0 +1,25 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Cache-Control', 's-maxage=3600');
+
+  const { cur, date } = req.query;
+
+  let url;
+  if (cur && date) {
+    const [y, m, d] = date.split('-');
+    url = `https://cbu.uz/uz/arkhiv-kursov-valyut/json/${cur}/${d}.${m}.${y}/`;
+  } else {
+    url = `https://cbu.uz/uz/arkhiv-kursov-valyut/json/`;
+  }
+
+  try {
+    const response = await fetch(url, {
+      headers: { 'User-Agent': 'Mozilla/5.0' }
+    });
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (e) {
+    res.status(500).json({ error: 'CBU API xatosi' });
+  }
+}
